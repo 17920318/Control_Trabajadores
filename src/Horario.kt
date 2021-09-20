1683d51fde4cdb0a7e7ac5a9b3edbb7d9e76e6c1
17920318 <lizbeth.271196@gmail.com>
2021-09-19 23:32:47 -0500

    Agregar

---
+++ src/Horario.kt
@@ -0,0 +1,11 @@
+class Horario(idHorario: Int, idPersonal: Int,  horaEntrada: String, horaSalida: String, dia: Int) {
+    public val idHorario = idHorario
+    public val idPersonal = idPersonal
+    public val horaEntrada = horaEntrada
+    public val horaSalida = horaSalida
+    public val dia = dia
+    
+}
+
+
+
